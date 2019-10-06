13c8a251750d4fd84a4d058bca02c158bb62464e
Kzyrgalina <anler.ind@gmail.com>
2019-10-06 23:46:06 +0300

    lesson_3_another_fib

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -88,7 +88,20 @@ fun digitNumber(n: Int): Int {
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = if (n > 2) fib(n - 1) + fib(n - 2) else 1
+fun fib(n: Int): Int {
+    var earlierFib = 0
+    var lastFib = 1
+    var plusFib: Int
+    var count = 0
+    var number = n
+    while (count < number - 1) {
+        plusFib = lastFib + earlierFib
+        earlierFib = lastFib
+        lastFib = plusFib
+        count++
+    }
+    return lastFib
+}
 
 /**
  * Простая
