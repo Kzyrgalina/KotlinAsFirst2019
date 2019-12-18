41d80d7827383d37e28b7237ccc6fe3dc82b1f09
Kzyrgalina <anler.ind@gmail.com>
2019-12-18 22:08:30 +0300

    less 8 (part)

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -79,14 +79,17 @@ data class Circle(val center: Point, val radius: Double) {
      * расстояние между их центрами минус сумма их радиусов.
      * Расстояние между пересекающимися окружностями считать равным 0.0.
      */
-    fun distance(other: Circle): Double = TODO()
+    fun distance(other: Circle): Double {
+        val dist = center.distance(other.center) - (radius + other.radius)
+        return if (dist >= 0.0) dist else 0.0
+    }
 
     /**
      * Тривиальная
      *
      * Вернуть true, если и только если окружность содержит данную точку НА себе или ВНУТРИ себя
      */
-    fun contains(p: Point): Boolean = TODO()
+    fun contains(p: Point): Boolean = center.distance(p) <= radius
 }
 
 /**
@@ -106,7 +109,22 @@ data class Segment(val begin: Point, val end: Point) {
  * Дано множество точек. Вернуть отрезок, соединяющий две наиболее удалённые из них.
  * Если в множестве менее двух точек, бросить IllegalArgumentException
  */
-fun diameter(vararg points: Point): Segment = TODO()
+fun diameter(vararg points: Point): Segment {
+    require(points.size >= 2)
+    var max = 0.0
+    var dist: Double
+    var current = 0 to 0
+    for (i in 0 until points.size - 1) {
+        for (j in i + 1 until points.size) {
+            dist = points[i].distance(points[j])
+            if (dist > max) {
+                max = dist
+                current = i to j
+            }
+        }
+    }
+    return Segment(points[current.first], points[current.second])
+}
 
 /**
  * Простая
